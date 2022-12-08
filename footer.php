8bb161376e5f9d2c4e3eb07815744fd953b906cc
DrakenBA2 <114453332+DrakenBA2@users.noreply.github.com>
2022-12-07 18:36:40 -0600

    Add files via upload
    PARA PODER UTILIZAR CREA UNA BASE DE DATOS E IMPORTAR EL SQL DE SCHEMA.SQL

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+</main>
+<footer class="px-2 py-2 fixed-bottom bg-dark">
+        <span class="text-muted">Attendance system with RFID tags and PHP. Written by
+            <a class="text-white" href=>hola perro</a>
+            &nbsp;|&nbsp;
+            <a target="_blank" class="text-white" href=https://www.youtube.com/watch?v=eKHlzHVPuGk">
+                View source
+            </a>
+        </span>
+</footer>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,29 @@
+
+<nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
+    <a class="navbar-brand" href="https://www.youtube.com/watch?v=qwsMT0zXhHw">
+        <img class="img-fluid" style="max-height: 50px" src="img/logo upt.png" loading="lazy">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" id="botonMenu" aria-label="Mostrar u ocultar menú">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="employees.php">Alumnos<i class="fa fa-users"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="employees_rfid.php">RFID &nbsp;<i class="fa fa-id-card"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="attendance_register.php">Registrar Asistencia<i class="fa fa-check-double"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="attendance_report.php">Reporte de asistencia <i class="fa fa-file-alt"></i></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href=https://www.youtube.com/watch?v=eKHlzHVPuGk>Suporte y ayuda&nbsp;<i class="fa fa-hands-helping"></i></a>
+            </li>
+        </ul>
+    </div>
+</nav>
